642e82b25f572a57f024d9a7fa9fae31777d7dde
Troy Kessler <troy.kessler99@gmail.com>
2025-04-09 17:11:21 +0200

    chore: new local test branch

--- src/features/wallet/context/StarknetWalletContext.tsx
+++ src/features/wallet/context/StarknetWalletContext.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Chain } from '@starknet-react/chains';
 import { StarknetConfig, publicProvider, voyager } from '@starknet-react/core';
 import { PropsWithChildren } from 'react';
@@ -53,3 +54,34 @@ export function StarknetWalletContext({ children }: PropsWithChildren<unknown>) 
     </StarknetConfig>
   );
 }
+=======
+// import { getStarknetChains } from "@hyperlane-xyz/widgets";
+// import {
+//   StarknetConfig,
+//   publicProvider,
+//   voyager
+// } from "@starknet-react/core";
+// import { PropsWithChildren, useMemo } from 'react';
+// import { InjectedConnector } from "starknetkit/injected";
+// import { useMultiProvider } from "../../chains/hooks";
+
+// export function StarknetWalletContext({ children }: PropsWithChildren<unknown>) {
+//   const multiProvider = useMultiProvider();
+//   const chains = getStarknetChains(multiProvider);
+//   const connectors = useMemo(() => [
+//     new InjectedConnector({ options: { id: "braavos", name: "Braavos" }}),
+//     new InjectedConnector({ options: { id: "argentX", name: "Argent X" }}),
+//   ], [])
+
+//    return (
+//     <StarknetConfig
+//       chains={chains}
+//       provider={publicProvider()}
+//       connectors={connectors}
+//       explorer={voyager}
+//     >
+//       {children}
+//     </StarknetConfig>
+//   );
+// }
+>>>>>>> Stashed changes
